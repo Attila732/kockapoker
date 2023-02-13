bc12b04808b55bd8cb86432711546c9edae0677d
kovács Attila <kovacsattila478@gmail.com>
2023-02-13 16:39:14 +0100

    poker

---
+++ src/Poker.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Poker {
+
+    public Poker() {
+        System.out.println(getPitch());
+        System.out.printf("%d %d %d %d %d\n",
+        getPitch(),
+        getPitch(),
+        getPitch(),
+        getPitch(),
+        getPitch()
+        );
+    }
+    public int getPitch(){
+        Random random =new Random();
+        int randInt=random.nextInt(6)+1;
+
+        return randInt;
+    }
+    
+}
